3d4f385d4eb77d46c5f16824ae6338bbd41e66b0
Thanos Prassas <tprassas@gmail.com>
2021-01-25 21:08:09 +0100

    Import Calendra  in tab 2

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -5,7 +5,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+// Calendar UI Module
+import { CalendarModule } from 'ion2-calendar';
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
@@ -14,8 +15,10 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
+    CalendarModule
   ],
+  
   declarations: [Tab2Page]
 })
 export class Tab2PageModule {}
