0e422265f2d8e970f32fe47dab6e995e7c89d32f
Thanos Prassas <tprassas@gmail.com>
2021-02-17 13:02:07 +0200

    resources change to be stored offline

--- src/app/resources-tab/resources.page.ts
+++ src/app/resources-tab/resources.page.ts
@@ -3,23 +3,23 @@ import { ResourcesService } from '../services/resources.service';
 import { Book } from '../shared/book';
 import { Movie } from '../shared/movie';
 import { Podcast } from '../shared/podcast';
-
+import resources from '../shared/resources.json'
 @Component( {
   selector: 'app-resources',
   templateUrl: 'resources.page.html',
   styleUrls: [ 'resources.page.scss' ]
 } )
 export class ResourcesPage {
-  podcasts: Podcast[];
+  podcasts: Podcast[] ;
   books: Book[];
   movies: Movie[];
   errMess: String;
-
+  resources: any = resources;
   constructor( private resoursesservice: ResourcesService,
     @Inject( 'BaseURL' ) private BaseURL ) { }
 
   ngOnInit() {
-    this.resoursesservice.getPodcasts()
+/*     this.resoursesservice.getPodcasts()
       .subscribe( podcasts => this.podcasts = podcasts,
         errmess => this.errMess = <any>errmess );
 
@@ -30,6 +30,6 @@ export class ResourcesPage {
     this.resoursesservice.getMovies()
       .subscribe( movies => this.movies = movies,
         errmess => this.errMess = <any>errmess );
-    
+ */
   }
 }
